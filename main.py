46ea851fec13be1d64f504e0020e14d2f7845565
fhchl <fhchl@users.noreply.github.com>
2024-10-27 21:11:53 +0100

    more rays for image

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ def main():
     )
     metal_mat = Material(color(0.8, 0.8, 0.8), reflectance=1.0)
     fuzzy_mat = Material(color(0.8, 0.6, 0.2), reflectance=1.0, fuzziness=0.5)
-    dielectric_mat = Material(color(1.0, 1.0, 1.0), 0., 0., 1.5, 1.0)
+    # dielectric_mat = Material(color(1.0, 1.0, 1.0), 0., 0., 1.5, 1.0)
     scene = Scene(
         objects=[
             Sphere(point(0.0, -100.5, -1), 100.0, yellow_mat),
             Sphere(point(0, 0.0, -1.2), 0.5, blue_mat),
-            Sphere(point(-1.0, 0.0, -1.0), 0.5, dielectric_mat),
+            Sphere(point(-1.0, 0.0, -1.0), 0.5, metal_mat),
             Sphere(point(1.0, 0.0, -1.0), 0.5, fuzzy_mat),
         ]
     )
@@ -30,7 +30,7 @@ def main():
         image_width=400,
         image_height=225,
         focal_length=1.0,
-        samples_per_pixel=100,
+        samples_per_pixel=400,
         sensor_height=2.0,
         max_depth=10,
     )
